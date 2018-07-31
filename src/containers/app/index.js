1ca7e4dc3fffca289d9b73f41e164f0f451c7aa8
Jakob Hyldtoft <jh@konform.com>
2018-07-31 16:41:00 +0200

    fix baseurl

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -12,42 +12,45 @@ import IconClash from './sword-clash.svg';
 
 import './app.css';
 
+console.log(process.env.REACT_APP_BASE_URL);
+
 const App = () => (
-	<div className="layout">
-		<BrowserRouter basename={process.env.REACT_APP_BASE_URL} />
-		<aside className="navigation">
-			<ol>
-				<li>
-					<Link to="/initiative">
-							<img src={IconInitiative} alt="Initiative Tracker" className="navigation__icon" />
-					</Link>
-				</li>
-				<li>
-					<Link to="/monsters">
-						<img src={IconMonster} alt="Monster Lookup" className="navigation__icon" />
-					</Link>
-				</li>
-				<li>
-					<Link to="/encounter">
-						<img src={IconClash} alt="Encounter Generator" className="navigation__icon" />
-					</Link>
-				</li>
-			</ol>
-		</aside>
+	<BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
+		<div className="layout">
+			<aside className="navigation">
+				<ol>
+					<li>
+						<Link to="/initiative">
+								<img src={IconInitiative} alt="Initiative Tracker" className="navigation__icon" />
+						</Link>
+					</li>
+					<li>
+						<Link to="/monsters">
+							<img src={IconMonster} alt="Monster Lookup" className="navigation__icon" />
+						</Link>
+					</li>
+					<li>
+						<Link to="/encounter">
+							<img src={IconClash} alt="Encounter Generator" className="navigation__icon" />
+						</Link>
+					</li>
+				</ol>
+			</aside>
 
 
-		<div className="layout-content">
-			<div className="layout-content__content">
-			<Switch>
-				<Redirect from="/" to="/initiative" exact />
-				<Route path="/initiative" component={InitiativeTracker}/>
-				<Route path="/monsters" component={MonsterLookup}/>
-				<Route path="/encounter" component={EncounterGenerator}/>
-				{/* <Route component={NoMatch}/> */}
-			</Switch>
+			<div className="layout-content">
+				<div className="layout-content__content">
+				<Switch>
+					<Redirect from="/" to="/initiative" exact />
+					<Route path="/initiative" component={InitiativeTracker}/>
+					<Route path="/monsters" component={MonsterLookup}/>
+					<Route path="/encounter" component={EncounterGenerator}/>
+					{/* <Route component={NoMatch}/> */}
+				</Switch>
+				</div>
 			</div>
 		</div>
-	</div>
+	</BrowserRouter>
 );
 
 export default App;
